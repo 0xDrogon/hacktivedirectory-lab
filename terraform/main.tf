1d8236776815233b4b7207afa1a7b64347474bfb
0xDrogon <tiagofsdomingues@gmail.com>
2023-01-11 20:37:10 +0000

    adds new vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,101 @@
-terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 4.16"
-        }
-    }
-
-    required_version = ">= 1.2.0"
-}
-
 provider "aws" {
     region     = "eu-west-1"
-    access_key = file(var.aws_access_key)
-    secret_key = file(var.aws_secret_key)
+    access_key = file(var.AWS_ACCESS_KEY_PATH)
+    secret_key = file(var.AWS_SECRET_KEY_PATH)
 }
 
-resource "aws_instance" "windows_server" {
-    ami           = "ami-0142f1ad576b2cc4d"
-    instance_type = "t2.small"
+# Our AWS keypair
+resource "aws_key_pair" "terraformkey" {
+    key_name   = "${terraform.workspace}-terraform-lab"
+    public_key = file(var.TERRAFORM_PUBLIC_KEY_PATH)
+}
+
+# Our VPC definition, using a default IP range of 10.0.0.0/16
+resource "aws_vpc" "lab-vpc" {
+    cidr_block           = var.VPC_CIDR
+    enable_dns_support   = true
+    enable_dns_hostnames = true
+}
+
+# Default route required for the VPC to push traffic via gateway
+resource "aws_route" "first-internet-route" {
+    route_table_id         = aws_vpc.lab-vpc.main_route_table_id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id             = aws_internet_gateway.lab-vpc-gateway.id
+}
+
+# Gateway which allows outbound and inbound internet access to the VPC
+resource "aws_internet_gateway" "lab-vpc-gateway" {
+    vpc_id = aws_vpc.lab-vpc.id
+}
+
+# Create our first subnet (Defaults to 10.0.1.0/24)
+resource "aws_subnet" "first-vpc-subnet" {
+    vpc_id = aws_vpc.lab-vpc.id
+
+    cidr_block        = var.FIRST_SUBNET_CIDR
+    availability_zone = "eu-west-1"
 
     tags = {
-        Name = "WindowsServer"
+        Name = "First Subnet"
     }
+}
+
+# Set DHCP options for delivering things like DNS servers
+resource "aws_vpc_dhcp_options" "first-dhcp" {
+    domain_name          = "ad-lab.local"
+    domain_name_servers  = [var.FIRST_DC_IP, var.PUBLIC_DNS]
+    ntp_servers          = [var.FIRST_DC_IP]
+    netbios_name_servers = [var.FIRST_DC_IP]
+    netbios_node_type    = 2
+
+    tags = {
+        Name = "First DHCP"
+    }
+}
+
+# Associate our DHCP configuration with our VPC
+resource "aws_vpc_dhcp_options_association" "first-dhcp-assoc" {
+    vpc_id          = aws_vpc.lab-vpc.id
+    dhcp_options_id = aws_vpc_dhcp_options.first-dhcp.id
+}
+
+# Our first domain controller of the "ad-lab.local" domain
+resource "aws_instance" "first-dc" {
+    ami                         = data.aws_ami.latest-windows-server.image_id
+    instance_type               = "t2.small"
+    key_name                    = aws_key_pair.terraformkey.key_name
+    associate_public_ip_address = true
+    subnet_id                   = aws_subnet.first-vpc-subnet.id
+    private_ip                  = var.FIRST_DC_IP
+    iam_instance_profile        = var.ENVIRONMENT == "deploy" ? null : aws_iam_instance_profile.ssm_instance_profile.0.name
+
+    tags = {
+        Workspace = "${terraform.workspace}"
+        Name = "${terraform.workspace}-First-DC"
+    }
+
+    vpc_security_group_ids = [
+        aws_security_group.first-sg.id,
+    ]
+}
+
+# Our second domain controller of the "ad-lab.local" domain
+resource "aws_instance" "second-dc" {
+    ami                         = var.ENVIRONMENT == "deploy" ? data.aws_ami.second-dc.0.image_id : data.aws_ami.latest-windows-server.image_id
+    instance_type               = "t2.small"
+    key_name                    = aws_key_pair.terraformkey.key_name
+    associate_public_ip_address = true
+    subnet_id                   = aws_subnet.first-vpc-subnet.id
+    private_ip                  = var.SECOND_DC_IP
+    iam_instance_profile        = var.ENVIRONMENT == "deploy" ? null : aws_iam_instance_profile.ssm_instance_profile.0.name
+
+    tags = {
+        Workspace = "${terraform.workspace}"
+        Name      = "${terraform.workspace}-Second-DC"
+    }
+
+    vpc_security_group_ids = [
+        aws_security_group.second-sg.id,
+    ]
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,7 +1,55 @@
-variable "aws_access_key" {
+variable "AWS_ACCESS_KEY_PATH" {
     default = "./keys/access_key"
 }
 
-variable "aws_secret_key" {
+variable "AWS_SECRET_KEY_PATH" {
     default = "./keys/secret_key"
 }
+
+variable "TERRAFORM_PUBLIC_KEY_PATH" {
+    default = "./keys/terraform_key.pub"
+}
+
+variable "TERRAFORM_PRIVATE_KEY_PATH" {
+    default = "./keys/terraform_key.pem"
+}
+
+variable "VPC_CIDR" {
+    default = "10.0.0.0/16"
+}
+
+variable "FIRST_SUBNET_CIDR" {
+    default = "10.0.1.0/24"
+}
+
+variable "FIRST_DC_IP" {
+    default = "10.0.1.101"
+}
+
+variable "SECOND_DC_IP" {
+    default = "10.0.1.102"
+}
+
+variable "PUBLIC_DNS" {
+    default = "1.1.1.1"
+}
+
+variable "MANAGEMENT_IPS" {
+    # Add in the public IP Address you will be hitting the cloud from, for example the public IP of your home address or VPN
+    default = ["1.2.3.4/32"]
+}
+
+variable "SSM_S3_BUCKET" {
+    # Add in the name of your S3 bucket like the example below
+    default = "this-is-just-a-fake-bucket"
+}
+
+data "http" "ip" {
+    url = "https://ifconfig.me/ip"
+}
+
+output "ip" {
+    value = data.http.ip.response_body
+}
+
+
